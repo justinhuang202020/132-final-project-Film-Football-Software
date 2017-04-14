9b2f3c397a20ffce2abd932d3c09959eaf418d7e
Justin Huang <jhuang11@cs.brown.edu>
2017-04-14 11:37:13 -0400

    start server

---
+++ back-end/server.js
@@ -0,0 +1,40 @@
+var express = require('express')
+var app = express();
+app.get('/creatTeam', function(request, response) {
+	response.render('creatTeam.html');
+});
+app.post('/createTeam/submit', submitTeam);
+function submitTeam(request, response) {
+	const password = request.body.password;
+	const name = request.body.name;
+	const email = request.body.email;
+	const team = request.body.team;
+	setUpAccount(email, password, team, name); 
+
+
+}
+function setUpAccount(email, password, team, name) {
+	let success = false;
+	firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  if (errorMessage!==undefined) {
+  	success = true;
+  }
+  // ...
+});
+	if (success) {
+		var user = firebase.auth().currentUser;
+		user.sendEmailVerification().then(function() {
+		createTeam(email, passowrd, team, name)
+  // Email sent.
+	}, function(error) {
+  // An error happened.
+});
+
+	}
+}
+function createTeam(email, password, team, name) {
+
+}
---
+++ front-end/login.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  $('#olvidado').click(function(e) {
+    e.preventDefault();
+    $('div#form-olvidado').toggle('500');
+  });
+  $('#acceso').click(function(e) {
+    e.preventDefault();
+    $('div#form-olvidado').toggle('500');
+  });
+});
+
+$(function() {
+    $('#login-form-link').click(function(e) {
+		$("#login-form").delay(100).fadeIn(100);
+ 		$("#register-form").fadeOut(100);
+		$('#register-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+	$('#register-form-link').click(function(e) {
+		$("#register-form").delay(100).fadeIn(100);
+ 		$("#login-form").fadeOut(100);
+		$('#login-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+});
+
+// submit button link to user homepage 
+// $(function() {
+//     $('#login-form-link').click(function(e) {
+// 		$("#login-form").delay(100).fadeIn(100);
+//  		$("#register-form").fadeOut(100);
+// 		$('#register-form-link').removeClass('active');
+// 		$(this).addClass('active');
+// 		e.preventDefault();
+// 	});
+// }); 
