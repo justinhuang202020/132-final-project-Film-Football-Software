755c4f7de44cc140626002406972c021f23befeb
ray3415 <raymond_holden@brown.edu>
2017-04-18 22:42:31 -0400

    JS and styling for adding traits in the front end

--- front-end/js/home.js
+++ front-end/js/home.js
@@ -1,22 +1,26 @@
-function loadSkillgraph() {
-$(".skillData").each(function(index, element) {
-    // element == this
-    var mydata = $(element).data();
-    var cnt = 0;
+$(document).ready(function(){
+	function loadSkillgraph() {
+	$(".skillData").each(function(index, element) {
+	    // element == this
+	    var mydata = $(element).data();
+	    var cnt = 0;
 
-    //recursive call with a time delay so user can see graph draw.
-    function go() {
-        if (cnt++ < mydata['percent']) {
-            setTimeout(go, 10);
-        }
-        $(element).css('width', cnt + '%');
+	    //recursive call with a time delay so user can see graph draw.
+	    function go() {
+		if (cnt++ < mydata['percent']) {
+		    setTimeout(go, 10);
+		}
+		$(element).css('width', cnt + '%');
 
-    }
+	    }
 
-    go();
+	    go();
 
+	});
+
+	}
+	loadSkillgraph();
+	$("#addTraitBtn").on('click', function(){
+		$('#newTraitLi').removeClass('hidden');	
+	});
 });
-
-}
-
-loadSkillgraph();
